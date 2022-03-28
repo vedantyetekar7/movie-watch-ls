03ea9e118cd7469625196c63c84683de2230b8a4
vedant <ursvedantyetekar@gmail.com>
2022-03-28 22:15:44 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import { Header } from "./components/Header.js";
+import { WatchList } from "./components/WatchList.js";
+import { Watched } from "./components/Watched.js";
+import { Add } from "./components/Add.js";
+import "./lib/font-awesome/css/all.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <WatchList />
+        </Route>
+        <Route path="/watched">
+          <Watched />
+        </Route>
+        <Route path="/add">
+          <Add />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
